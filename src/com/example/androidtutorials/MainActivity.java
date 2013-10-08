500cb0be6ecbb90040aade99d3d39a5616c2374c
Zhang Franklin <jfzhang@cienet.com.cn>
2013-10-08 14:06:49 +0800

    Add TAG

--- src/com/example/androidtutorials/MainActivity.java
+++ src/com/example/androidtutorials/MainActivity.java
@@ -5,11 +5,13 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+	private final String TAG = "AndroidTutorials";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
 	}
 
 	@Override
