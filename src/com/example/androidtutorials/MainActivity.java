4de28623e3efffb2e90f3a68c430e4d9493c6feb
Zhang Franklin <jfzhang@cienet.com.cn>
2013-10-08 14:15:25 +0800

    Add log

--- src/com/example/androidtutorials/MainActivity.java
+++ src/com/example/androidtutorials/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.androidtutorials;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,7 +11,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		Log.d(TAG, "onCreate");
 		
 	}
 
