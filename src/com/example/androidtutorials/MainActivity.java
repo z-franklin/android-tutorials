34649c7475ee32c67380caf3d96cde960272d62a
Zhang Franklin <jfzhang@cienet.com.cn>
2013-10-08 14:22:00 +0800

    Add Button1

--- src/com/example/androidtutorials/MainActivity.java
+++ src/com/example/androidtutorials/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	private final String TAG = "AndroidTutorials";
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		Log.d(TAG, "onCreate");
+		
+		Button b1 = (Button)findViewById(R.id.button1); 
 		
 	}
 
